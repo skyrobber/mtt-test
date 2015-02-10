24dcb4083ebc8c7402a87e8f9115232c80a9449e
root <root@ip-172-31-44-171.eu-west-1.compute.internal>
2015-02-10 07:45:09 +0000

    Auto commit on Tue Feb 10 07:45:09 UTC 2015

--- reports.sh
+++ reports.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Getting the page to a file
-curl -o /var/www/html/mtt-test/index-orig.html http://www.met.ie/latest/reports.asp
+loc="/var/www/html/mtt-test"
+curl -o $loc/index-orig.html http://www.met.ie/latest/reports.asp
 
 # Replacing text
-sed -e "s_"/images/met.ie-logo.gif"_"http://www.met.ie/images/met.ie-logo.gif"_g" index-orig.html > index.html
+sed -e "s_"/images/met.ie-logo.gif"_"http://www.met.ie/images/met.ie-logo.gif"_g" $loc/index-orig.html > $loc/index.html
 rm -rf /var/www/html/mtt-test/index-orig.html
 
 # Commit the changes locally
-loc="/var/www/html/mtt-test"
 cd $loc
 git add .
 git commit -m "Auto commit on `date`"
