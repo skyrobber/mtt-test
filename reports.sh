b3b14681d82e8eed7e6ab32d8097cb02cf1c732d
root <root@ip-172-31-44-171.eu-west-1.compute.internal>
2015-02-10 05:47:54 +0000

    Auto Update on Tue Feb 10 05:47:54 UTC 2015

--- reports.sh
+++ reports.sh
@@ -5,3 +5,13 @@ curl -o /var/www/html/mtt-test/index-orig.html http://www.met.ie/latest/reports.
 # Replacing text
 sed -e "s_"/images/met.ie-logo.gif"_"http://www.met.ie/images/met.ie-logo.gif"_g" index-orig.html > index.html
 rm -rf /var/www/html/mtt-test/index-orig.html
+
+# Commit the changes locally
+loc="/var/www/html/mtt-test"
+cd $loc
+git add .
+git commit -m "Auto Update on `date`"
+
+# Push the changes to github
+git push origin master
+
